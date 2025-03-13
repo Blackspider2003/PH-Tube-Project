643f1c2067566e6906add3c3e356fe5f8108c29e
Blackspider2003 <mdbishalahmedrafi55@gmail.com>
2025-03-13 22:09:20 +0600

    Butto Display Done

--- first.js
+++ first.js
@@ -7,7 +7,30 @@ function loadCategories (){
                                 // --- send the data to Display
                                 .then((data) =>displayCategories(data.categories));
 }
+
+// sample things
+// {
+//                 "category_id": "1001",
+//                 "category": "Music"
+//                 }
+
+
+
 function displayCategories (categories){
-                console.log(categories)
+
+                // --- Get the Container ---
+               const categoryContainer = document.getElementById('category-container');
+
+               // --- loop operation of array of object ---
+               for(cat of categories){
+                console.log(cat);
+
+                // --- create element ---
+                const categoryDiv =document.createElement('div');
+               categoryDiv.innerHTML = `<button class="btn btn-sm hover:bg-red-600 hover:text-white">${cat.category}</button>`;
+
+               // --- append the created element ---
+               categoryContainer.append(categoryDiv);
+               }  
 }
 loadCategories();
